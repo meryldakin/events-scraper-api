ceaeadb5e23a8d9491597695403c42ea69dc7180
meryldakin <meryl.k.dakin@gmail.com>
2017-11-19 15:55:12 -0500

    Scrapes for images

--- app/helpers/importer.rb
+++ app/helpers/importer.rb
@@ -9,18 +9,20 @@ class Importer
         while parse_page.css('.feature-item')[$i]  do
           # while $i < $num  do
             name = parse_page.css('.feature-item')[$i].css('.feature-item__content').css('.feature-item__column').css('h3').children.text.strip
-            
+
             url = "https://www.timeout.com" + parse_page.css('.feature-item')[$i].css('.feature-item__content').css('.feature-item__column').css('a[data-tracking]')[0]['href']
-            
+
           description = parse_page.css('.feature-item')[$i].css('.feature-item__content').css('.feature-item__column').css('div.feature_item__annotation--truncated').css('p').text
+          
+          image = parse_page.css('.feature-item')[$i].css('.feature-item__content').css('.feature-item__column--image').css('.image_wrapper').children[1] ? parse_page.css('.feature-item')[$i].css('.feature-item__content').css('.feature-item__column--image').css('.image_wrapper').children[1].attributes['src'].value : nil
 
           date = parse_page.css('.feature-item')[$i].css(' .listing_meta_controls > .listings_flags_bar > .listings_flags > .icon_calendar').text.strip
 
-          Event.create(name: name, url: url, description: description, date: date, month: month, source_id: 1)
+          Event.create(name: name, url: url, description: description, date: date, month: month, source_id: 1, image_url: image)
 
           $i += 1
         end
-    
+
       end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171021143017) do
+ActiveRecord::Schema.define(version: 20171119192627) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20171021143017) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "source_id"
+    t.string "image_url"
   end
 
   create_table "sources", force: :cascade do |t|
